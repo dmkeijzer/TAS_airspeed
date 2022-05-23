681ff57ae94c604dfa53d97809279de3e3527d4e
svanteylingen <s.vanteylingen@gmail.com>
2022-05-23 10:07:01 +0200

    I love TensorFlow

--- Neural network/run_model.py
+++ Neural network/run_model.py
@@ -8,30 +8,28 @@ from tensorflow.keras import layers
 
 model = keras.models.load_model(r"C:\Users\Stijn van Teylingen\OneDrive - Delft University of Technology\Test, Analysis & Simulation\Python_AI\TAS_airspeed\Neural network\Model")
 
-path = r"C:\Users\Stijn van Teylingen\OneDrive - Delft University of Technology\Test, Analysis & Simulation\Python_AI\TAS_airspeed\data_sets\tensor_file.csv"
+path = r"C:\Users\Stijn van Teylingen\OneDrive - Delft University of Technology\Test, Analysis & Simulation\Python_AI\TAS_airspeed\data_sets\tensor_file_raw1.csv"
 
 df_tens = pd.read_csv(path).to_numpy()
 df_tens = np.delete(df_tens, 0, axis=1)
+df_tens = df_tens.transpose()
 
 
+x_sets = df_tens[:,:-1]
+x_eval = x_sets[102:130,:]
+x_test = x_sets[0:102,:]
 
-x_sets = df_tens[:-1,:]
-x_eval = x_sets[:,:102]
-x_test = x_sets[:,102:].transpose()
-
-y_sets = df_tens[-1,:]
-y_eval= y_sets[:102]
-y_test = y_sets[102:].transpose()
-
-x_val = x_eval[:,89:].transpose()
-x_train = x_eval[:,:89].transpose()
-y_val = y_eval[89:].transpose()
-y_train = y_eval[:89].transpose()
+y_sets = df_tens[:,-1]
+y_eval= y_sets[102:130]
+y_test = y_sets[0:102]
 
 print("Evaluate on test data")
-results = model.evaluate(x_test, y_test, batch_size=128)
-print("test loss, test acc:", results)
+eval = model.evaluate(x_eval, y_eval, batch_size=128)
+print("test loss, test acc:", eval)
 
 print("Generate predictions for all samples")
-predictions = model.predict(x_test)
-print("predictions shape:", predictions.shape)
+predictions = model.predict(x_sets)
+results = pd.DataFrame(predictions, columns=["estimated values"])
+results["true speed"] = y_sets
+
+print("predictions shape:", results)
